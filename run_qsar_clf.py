0a77f0929de185ca878521fb014ffc96169b3b55
Hyper-Flow <tqichun@gmail.com>
2020-05-27 14:51:19 +0800

    add run_qsar_clf.py

--- run_qsar_clf.py
+++ run_qsar_clf.py
@@ -10,7 +10,7 @@ import joblib
 from sklearn.model_selection import KFold
 
 import autoflow
-from autoflow import AutoFlowRegressor, HDL_Constructor
+from autoflow import HDL_Constructor, AutoFlowClassifier
 from autoflow.tuner import Tuner
 
 try:
@@ -22,33 +22,37 @@ except Exception:
 
 @click.command()
 @click.option("--file", "-f", help="input file name", type=click.Path())
-@click.option("--inst", "-i", help="instance id",default=0, type=int)
+@click.option("--inst", "-i", help="instance id", default=0, type=int)
 @click.option("--target", "-t", help="target column name", type=str)
 @click.option("--ignore", "-g", help="ignore column name", type=str, multiple=True)
 @click.option("--id", help="id column name", type=str)
-@click.option("--n_jobs","-n",default=5,  help="n_jobs_in_algorithm", type=str)
+@click.option("--n_jobs", "-n", default=5, help="n_jobs_in_algorithm", type=str)
 def main(file, inst, target, ignore, id, n_jobs):
     if inst is None:
-        inst=0
+        inst = 0
     examples_path = Path(autoflow.__file__).parent.parent / "data"
     # 加载pickle格式的数据
     train_df = joblib.load(examples_path / file)
     # train_df = load("qsar")
     estimators = [
-        "adaboost", "bayesian_ridge",
-        "catboost",
-        "decision_tree", "elasticnet", "extra_trees",
-        # "gradient_boosting",
+        "adaboost",
+        "decision_tree",
+        "extra_trees",
         "k_nearest_neighbors",
+        "liblinear_svc",
+        "libsvm_svc",
         "lightgbm"
+        "logistic_regression"
+        "random_forest"
+        "sgd"
     ]
 
     hdl_constructor = HDL_Constructor(
         DAG_workflow={
             "num->selected": {
-                "_name": "select.from_model_reg",
+                "_name": "select.from_model_clf",
                 "_vanilla": True,
-                "estimator": "sklearn.ensemble.ExtraTreesRegressor",
+                "estimator": "sklearn.ensemble.ExtraTreesClassifier",
                 "n_estimators": 10,
                 "max_depth": 7,
                 "min_samples_split": 10,
@@ -65,19 +69,22 @@ def main(file, inst, target, ignore, id, n_jobs):
 
     tuners = [
         Tuner(
-            search_method="beam",
+            search_method="random",
             search_method_params={
-                "beam_steps": [
-                    {
-                        "estimating": estimators,
-                        "num->selected.*_select_percent": [10 + inst * 5, 30 + inst * 5],
-                    },
-                    {
-                        "num->selected.*_select_percent": [i for i in range(10, 70, 10)]
-                    }
-                ]
+                "specific_allocate": {
+                    ("estimating", "adaboost"): 1,
+                    ("estimating", "decision_tree"): 2,
+                    ("estimating", "extra_trees"): 5,
+                    ("estimating", "k_nearest_neighbors"): 1,
+                    ("estimating", "liblinear_svc"): 1,
+                    ("estimating", "libsvm_svc"): 1,
+                    ("estimating", "lightgbm"): 10,
+                    ("estimating", "logistic_regression"): 5,
+                    ("estimating", "random_forest"): 5,
+                    ("estimating", "sgd"): 2,
+                }
             },
-            debug=False,
+            # run_limit=50,
             per_run_time_limit=1200,
             per_run_memory_limit=30 * 1024,
             n_jobs_in_algorithm=n_jobs
@@ -93,7 +100,7 @@ def main(file, inst, target, ignore, id, n_jobs):
         )
     ]
 
-    trained_pipeline = AutoFlowRegressor(
+    trained_pipeline = AutoFlowClassifier(
         consider_ordinal_as_cat=False,
         tuner=tuners,
         hdl_constructor=hdl_constructors,
@@ -121,10 +128,10 @@ def main(file, inst, target, ignore, id, n_jobs):
         splitter=KFold(n_splits=5, shuffle=True, random_state=0),
         fit_ensemble_params=False,
         dataset_metadata={
-            "name":file
+            "name": file
         },
         task_metadata={
-            "instance_id":inst
+            "instance_id": inst
         },
         specific_task_token=str(inst)
         # is_not_realy_run=True
