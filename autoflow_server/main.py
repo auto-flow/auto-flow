087777a0866a0308e0f1e88f689c48158cdc82a3
Hyper-Flow <tqichun@gmail.com>
2020-06-09 12:03:46 +0800

    finish refactor resource_manager to add execute layer in database operate

--- autoflow_server/main.py
+++ autoflow_server/main.py
@@ -1,17 +1,27 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Author  : qichun tang
-# @Contact    : tqichun@gmail.com
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+class Item(BaseModel):
+    name: str
+    description: str = None
+    price: float
+    tax: float = None
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Foo",
+                "description": "A very nice Item",
+                "price": 35.4,
+                "tax": 3.2,
+            }
+        }
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.put("/items/{item_id}")
+async def update_item(*, item_id: int, item: Item):
+    results = {"item_id": item_id, "item": item}
+    return results
